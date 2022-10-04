3042d9268cdf75a1c51267afe04795768309fc04
krivalex <aleksei_krivoborodov@mail.ru>
2022-10-04 21:06:04 +0600

    react-deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '5633382457:AAFH8Ds-8GkGn9R-gPx-xPkpyRdWb22ofHM'
-const webAppUrl = '/'
+const webAppUrl = 'https://633c4a8af6ae0b3198aa655f--jocular-kitten-5df275.netlify.app/'
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -10,17 +10,17 @@ bot.on('message', async (msg) => {
     const text = msg.text
 
     if(text === '/start') {
-        // await bot.sendMessage(chatId, "Look at the buttons and forms, dumbells", {
-        //     reply_markup: {
-        //         inline_keyboard: [
-        //             [{text: "Заполнить форму", web_app: {url: webAppUrl}}],
-        //             [{text: "Нажать на кнопку", web_app: {url: webAppUrl}}],
-        //             [{text: "Просто есть", web_app: {url: webAppUrl}}]
-        //         ]
-        //     }
-        // })
+        await bot.sendMessage(chatId, "Look at the inline, dumbells", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: "Заполнить форму", web_app: {url: webAppUrl}}],
+                    [{text: "Нажать на кнопку", web_app: {url: webAppUrl}}],
+                    [{text: "Просто есть", web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
 
-        await bot.sendMessage(chatId, "Look at the buttons and forms, dumbells", {
+        await bot.sendMessage(chatId, "Look at the keyboard, dumbells", {
             reply_markup: {
                 keyboard: [
                     [{text: "Заполнить форму"}],
